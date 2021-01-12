3792431d78e96ff719e4b802c852da00e3d19c3d
Thilina <hellowthilinafive@gmail.com>
2021-01-13 01:21:09 +0530

    first commit

---
+++ App/Controllers/AuthController.cs
@@ -0,0 +1,47 @@
+﻿using App.Service;
+using App.Shared;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace App.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+
+        private IUserService _userService;
+        
+        public AuthController(IUserService userService)
+        {
+            _userService = userService;
+          
+        }
+
+        [HttpGet]
+
+        public string get() {
+            return "pengu";
+        }
+
+        // /api/auth/register
+        [HttpPost]
+        public async Task<IActionResult> RegisterAsync([FromBody] RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _userService.RegisterUserAsync(model);
+
+                if (result.IsSuccess)
+                    return Ok(result); // Status Code: 200 
+
+                return BadRequest(result);
+            }
+
+            return BadRequest("Some properties are not valid"); // Status code: 400
+        }
+    }
+}
---
+++ App/Service/IUserService.cs
@@ -0,0 +1,63 @@
+﻿using App.Shared;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace App.Service
+{
+    public interface IUserService
+    {
+        Task<UserManagerResponse> RegisterUserAsync(RegisterViewModel model);
+    }
+
+    public class UserService : IUserService
+    {
+        private UserManager<IdentityUser> _userManger;
+
+        public UserService(UserManager<IdentityUser> userManager)
+        {
+            _userManger = userManager;
+
+        }
+
+        public async Task<UserManagerResponse> RegisterUserAsync(RegisterViewModel model)
+        {
+            if (model == null)
+                throw new NullReferenceException("Reigster Model is null");
+
+            if (model.Password != model.ConfirmPassword)
+                return new UserManagerResponse
+                {
+                    Message = "Confirm password doesn't match the password",
+                    IsSuccess = false,
+                };
+
+            var identityUser = new IdentityUser
+            {
+                Email = model.Email,
+                UserName = model.Email,
+            };
+
+            var result = await _userManger.CreateAsync(identityUser, model.Password);
+
+            if (result.Succeeded)
+            {
+                return new UserManagerResponse
+                {
+                    Message = "User created successfully!",
+                    IsSuccess = true,
+                };
+            }
+
+            return new UserManagerResponse
+            {
+                Message = "User did not create",
+                IsSuccess = false,
+                Errors = result.Errors.Select(e => e.Description)
+            };
+
+        }
+    }
+}
