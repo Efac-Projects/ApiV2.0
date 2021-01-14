086c515aa469a81d4990328808c23c4f3e46b934
Kavindi <kavithyangisiribaddana2016@gmail.com>
2021-01-14 23:14:42 +0530

    creat model classes

---
+++ App/Models/Review.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace App.Models
+{
+    public class Review
+    {
+        public int ReviewId { get; set; }
+        public int Appointment { get; set; }
+        public string Comment { get; set; }
+    }
+}
---
+++ App/Models/Service.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace App.Models
+{
+    public class Service
+    {
+        public int ServiceId { get; set; }
+        public int ServiceName { get; set; }
+        public int BusinessId { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime  { get; set; }
+        public string Summary { get; set; }
+        public string ServiceOwner { get; set; }
+        public bool IsAvailable { get; set; }
+
+    }
+}
